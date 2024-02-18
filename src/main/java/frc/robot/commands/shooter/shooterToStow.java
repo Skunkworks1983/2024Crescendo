71c99b4c81f12c1984701e09cd036371d50dfa62
TacoLord346 <TacoLord346@gmail.com>
2024-02-17 16:33:21 -0800

    command

---
+++ src/main/java/frc/robot/commands/shooter/shooterToStow.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.shooter;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.Constants;
+import frc.robot.subsystems.Shooter;
+
+public class shooterToStow extends Command {
+  
+  public Shooter shooter = Shooter.getInstance();
+
+  public shooterToStow() {
+    
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    Rotation2d shooterAngle = new Rotation2d(Units.degreesToRadians(Constants.Shooter.SHOOTER_RESTING_POSITION_DEGREES) );
+    
+    shooter.setShooterAngle(shooterAngle);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    if(shooter.getShooterPivotRotation() >= Constants.Shooter.SHOOTER_RESTING_POSITION_DEGREES) {
+      shooter.SetShooterAngleVelocity(-0.087);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    
+    return shooter.getLimitSwitchOutput(false);
+  }
+}
