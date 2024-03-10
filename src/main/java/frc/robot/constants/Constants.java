86b019a4e6503bca6589235c0ba95b3c7bffaa57
TacoLord346 <TacoLord346@gmail.com>
2024-03-10 14:38:40 -0700

    pre finals match

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -210,7 +210,7 @@ public class Constants {
     public static final double AMP_FLYWHEEL_SPEED = 20;
     public static final double DEFUALT_SPEAKER_FLYWHEEL_SPEED = 27.0;
     public static final double PODIUM_FLYWHEEL_SPEED = 27;
-    public static final double PASS_FLYWHEEL_SPEED = 21.6;
+    public static final double PASS_FLYWHEEL_SPEED = 23;
 
     public static final double PODIUM_ANGLE_DEGREES = 50;
     public static final double PASS_ANGLE_DEGREES = 50;
