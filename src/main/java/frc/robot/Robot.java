6c40e34fd83c58bdd0df252a1de4820a7b4895d2
TacoLord346 <TacoLord346@gmail.com>
2024-03-17 16:05:39 -0700

    added a... camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,7 +6,7 @@ package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
-
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -49,6 +49,8 @@ public class Robot extends TimedRobot {
     shooter = Shooter.getInstance();
     indexer = Indexer.getInstance();
     collector = Collector.getInstance();
+    //enable the driver camera
+    CameraServer.startAutomaticCapture();
 
   //wait times
   NamedCommands.registerCommand("WaitOneSecond", new WaitDuration(1.0));
