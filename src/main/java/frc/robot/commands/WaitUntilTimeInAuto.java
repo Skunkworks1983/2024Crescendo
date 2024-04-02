496bc6653b2d9f0c6b7025019916b7c4ac1fa081
TacoLord346 <TacoLord346@gmail.com>
2024-04-01 23:05:38 -0700

    added logs

--- src/main/java/frc/robot/commands/WaitUntilTimeInAuto.java
+++ src/main/java/frc/robot/commands/WaitUntilTimeInAuto.java
@@ -18,7 +18,9 @@ public class WaitUntilTimeInAuto extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    System.out.println("Wait Until Time In Auto Command Initialize");
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -26,7 +28,9 @@ public class WaitUntilTimeInAuto extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    System.out.println("Wait Until Time In Auto Command End");
+  }
 
   // Returns true when the command should end.
   @Override
