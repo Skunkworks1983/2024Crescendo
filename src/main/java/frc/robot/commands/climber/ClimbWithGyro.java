c4662161efe4c5a0e2431ef721b589fdc881694e
bella <bellasoutham@gmail.com>
2024-10-01 19:58:58 -0700

    fixed smartclimb

--- src/main/java/frc/robot/commands/climber/ClimbWithGyro.java
+++ src/main/java/frc/robot/commands/climber/ClimbWithGyro.java
@@ -59,6 +59,8 @@ public class ClimbWithGyro extends Command {
   @Override
   public void end(boolean interrupted) {
     System.out.println("Climb With Gyro Command End");
+    climber.setClimberOutput(ClimbModule.LEFT, -0.02);
+    climber.setClimberOutput(ClimbModule.RIGHT, -0.02);
     climber.setBrakeMode(ClimbModule.LEFT);
     climber.setBrakeMode(ClimbModule.RIGHT);
     climber.applySlotConfig(ClimbModule.LEFT, ClimberSlotConfigs.SLOT_1);
@@ -73,8 +75,17 @@ public class ClimbWithGyro extends Command {
     // command
     if (climber.getClimberPostition(ClimbModule.LEFT) <= ClimberConstants.MIN_POSITION
         || climber.getClimberPostition(ClimbModule.RIGHT) <= ClimberConstants.MIN_POSITION) {
+          
+          System.out.println("left climber pos " + climber.getClimberPostition(ClimbModule.LEFT));
+    System.out.println("right climber pos " + climber.getClimberPostition(ClimbModule.RIGHT));
+    System.out.println("interupted");
       return true;
+
+      
     }
+
+    System.out.println("left climber pos " + climber.getClimberPostition(ClimbModule.LEFT));
+    System.out.println("right climber pos " + climber.getClimberPostition(ClimbModule.RIGHT));
 
     return false;
   }
